d0246e8835a21f607236697334fc096fafe96caa
Matthew Courtney <matthewdc182@gmail.com>
2020-12-17 23:04:23 -0500

    API key and the ajax call work using a preprogrammed query, need to add the search number ability also just console,loggig the array now

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-window.onload = function() {
-    console.log("hello")
-}
+
+$("#search").on("click", function() {
+    event.preventDefault();
+    console.log("clicked");
+
+    var apiKey = "VMU7yTXM4dOt0QRT8qkBXND2EMSS6od6";
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + apiKey;
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        var docs = response.response.docs;
+        console.log(docs);
+    })
+})
